f359b76f7040d7c65146e2728ace59c0ea6b510f
Stephen Mann <thanthese@gmail.com>
2014-04-04 18:29:23 -0500

    added keys and overflows

--- print_calendar.py
+++ print_calendar.py
@@ -1,8 +1,10 @@
 import datetime
+import operator
 
 
 SUNDAY = 6
-MONTHS = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+MONTH = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+WEEKDAY = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
 
 
 def min_sunday(date):
@@ -21,7 +23,8 @@ def print_calendar(events_index, cell_width, cell_height):
     while sunday < max_date:
         print header(sunday, cell_width)
         for lineid in xrange(cell_height):
-            print row(events_index, sunday, lineid, cell_width)
+            print row(events_index, sunday, lineid, cell_width, cell_height)
+        print_week_summary(events_index, sunday, cell_width)
         sunday += datetime.timedelta(days=7)
 
 
@@ -29,23 +32,52 @@ def header(sunday, cell_width):
     hstr = ""
     for i in range(7):
         day = sunday + datetime.timedelta(days=i)
-        datestr = MONTHS[day.month - 1] + " " + str(day.day)
+        if day == datetime.date.today():
+            datestr = "TODAY!"
+        else:
+            datestr = "{} {}/{}".format(WEEKDAY[day.weekday()], day.month, day.day)
         hstr += datestr.ljust(cell_width)
     return hstr
 
 
-def row(events_index, sunday, lineid, cell_width):
+def row(events_index, sunday, lineid, cell_width, cell_height):
     rowstr = ""
     for i in xrange(7):
         day = sunday + datetime.timedelta(days=i)
-        rowstr += cell(events_index, day, lineid, cell_width).ljust(cell_width)
+        rowstr += cell(events_index, day, lineid, cell_width, cell_height).ljust(cell_width)
     return rowstr
 
 
-def cell(events_index, date, lineid, cell_width):
-    if date not in events_index or lineid > len(events_index[date]) - 1:
+def cell(events_index, date, lineid, cell_width, cell_height):
+    if date not in events_index:
         return ""
+
     event_list = events_index[date]
+    out_of_events = lineid > len(events_index[date]) - 1
+    if out_of_events:
+        return ""
+
+    lastline = lineid + 1 == cell_height
+    overflow = len(event_list) > cell_height
+    if lastline and overflow:
+        return '[' + ", ".join([str(e.id) for e in event_list[lineid:]]) + ']'
+
     event = event_list[lineid]
-    desc = event.desc[0:cell_width - 1]
-    return desc
+    return "{}: {}".format(event.id, event.desc)[0:cell_width - 1]
+
+def flatten(list_of_lists):
+    if len(list_of_lists) == 0:
+        return []
+    return reduce(operator.add, list_of_lists)
+
+def print_week_summary(events_index, sunday, cell_width):
+    format = lambda e: "[{}]: #{} {}".format(e.id, e.type, e.desc.strip())
+    days = [sunday + datetime.timedelta(days=i) for i in range(7)]
+    events = flatten([events_index[d] for d in days if d in events_index])
+    keys = sorted(list(set(map(format, events))))
+    if len(keys) > 0:
+        width = cell_width * 7
+        print "----/ key /".ljust(width, '-')
+        for k in keys:
+            print k
+        print "".ljust(width, '-')
